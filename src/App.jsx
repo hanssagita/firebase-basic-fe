489b1bfff775cd048265f08102541cb783a42c61
hans.soegiarto <hans.sagita@gmail.com>
2022-12-27 17:15:03 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Auth from './components/Auth'
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-5xl">Firebase Tutorial</h1>
+      <Auth />
+    </div>
+  )
+}
+
+export default App
